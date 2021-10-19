a3ba4e3b4300ff47b8f5971d06aa5943c266c74f
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-19 22:10:21 +0530

    Refactored :Indentation Correction

--- linkedlistpracticeproblems/Node.java
+++ linkedlistpracticeproblems/Node.java
@@ -1,34 +1,28 @@
 package linkedlistpracticeproblems;
 
-	public class Node<K> implements INode<K> {
-		private K key;
-		private Node next;
+public class Node<K> implements INode<K> {
+	private K key;
+	private Node next;
 		
-		public Node(K key) {
-			this.key = key;
-			this.next = null; 
-		}
+	public Node(K key) {
+	          this.key = key;
+		  this.next = null; 
+	}
 
-		public K getKey() {
-			return key;
-		}
+	public K getKey() {
+		return key;
+	}
+		
+	public void setKey(K key) {
+		this.key = key;
+	}
 
+	public INode<K> getNext() {
+		return next;
+	}
 		
-		public void setKey(K key) {
-			this.key = key;
-		}
-
-	
-		public INode<K> getNext() {
-			return next;
-		}
-
-		
-		public void setNext(INode next) {
-			this.next = (Node<K>) next;
-			
-		}	
-		
-		
+	public void setNext(INode next) {
+		this.next = (Node<K>) next;
+	}	
 }
 
